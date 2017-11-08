3fc52439ab174443417360b78acceef773a95c4e
David Aviles <aviles.david1@gmail.com>
2017-11-08 14:49:00 +0000

    fix to views alignmnet

--- resources/views/client/create.blade.php
+++ resources/views/client/create.blade.php
@@ -6,7 +6,7 @@ New Client
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2-dash">
+        <div class="col-md-8">
             <div class="panel panel-default">
                 <div class="panel-heading">Add New Client</div>
 
