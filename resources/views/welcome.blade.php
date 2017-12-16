9393dafa4d34bb68cc6a3fbba0c956804d7c6641
David Aviles <aviles.david1@gmail.com>
2017-12-16 00:59:07 -0500

    updated index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,21 @@
 @extends('layouts.app')
 
 @section('title')
-    A Simple Invoice Generator
+A Simple Invoice Generator
 @endsection
 
 @section('content')
-    <div class="flex-center position-ref full-height">
-           <div class="content">
-                <div class="title">
-                    <h1 class="red">InvoiceMe App</h1>
-                </div>
+<div class="flex-center position-ref full-height">
+ <div class="content">
+    <div class="title">
+        <h1 class="red">InvoiceMe</h1>
+    </div>
 
-                <div class="links m-b-md">
-                    Register or Login to Access Features
-                </div>
-                <p class="text-center">Under Construction</p>
-            </div>
-        </div>
+    <div class="links m-b-md">
+        <p>A simple application for managing clients, services and payments.</p>
+        <strong>Register or Login to Access Features</strong>
+    </div>
+    <p class="text-center">Under Construction</p>
+</div>
+</div>
 @endsection
